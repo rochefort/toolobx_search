4229a33d6239884f62a55c85a80c041b3d25a96f
rochefort <terasawan@gmail.com>
2011-02-22 01:32:51 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToolboxSearch::Application.config.secret_token = 'd14b274fe8a9a063a1d34e35d40ad66b35d37e9d4700a6c1aa0ea1848e4609a9a892d8c1bafbd75d66e1e1aec91a021aad5bb831ecefdab3e7b9ff4016199fd8'
