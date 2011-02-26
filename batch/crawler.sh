b71571bbb7895d6d06f75e6dff1eaaf0d1a70817
rochefort <terasawan@gmail.com>
2011-02-27 01:42:31 +0900

    modified crawler script

--- batch/crawler.sh
+++ batch/crawler.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#cd /srv/rails/toolbox_search/current
-rails runner -e development "ToolboxSearch::Crawler.new.crawl" 
-#/usr/local/bin/ruby script/runner -e production "ToolboxSearch::Crawler.new.crawl" > log/cron_crawler.log 2>&1
+cd /srv/rails/toolbox_search/current
+#/usr/local/bin/rails runner -e development "ToolboxSearch::Crawler.new.crawl"
+/usr/local/bin/rails runner -e production "ToolboxSearch::Crawler.new.crawl" > log/cron_crawler.log 2>&1
 /usr/bin/touch tmp/restart.txt
