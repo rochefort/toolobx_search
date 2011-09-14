3897dd59c4254f036f30c0ff4023f478e01133b2
rochefort <teraswan@gmail.com>
2011-09-14 22:37:51 +0900

    upgrade Rails3.1.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ ToolboxSearch::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  # config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -55,4 +56,6 @@ ToolboxSearch::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.assets.precompile += ['base.css']
 end
