e2c6e72ebc40e0aa262ebd6cd464237a6f29ac14
rochefort <teraswan@gmail.com>
2011-09-18 16:33:01 +0900

    bug: batch logging

--- batch/crawler.sh
+++ batch/crawler.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 export PATH=/usr/local/bin/:$PATH
 bundle_bin=/home/trsw/.gem/ruby/1.9.1/bin/bundle
-
 log_file=log/cron_crawler.log
-date "+%Y%m%d %H:%M:%S" > $log_file 2>&1
 
 cd /usr/local/rails/toolbox_search/current
+date "+%Y%m%d %H:%M:%S" > $log_file 2>&1
 $bundle_bin exec rails runner -e production "ToolboxSearch::Crawler.new.crawl" >> $log_file 2>&1
 /bin/touch tmp/restart.txt
--- batch/indexer.sh
+++ batch/indexer.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 bundle_bin=/home/trsw/.gem/ruby/1.9.1/bin/bundle
-
 log_file=log/cron_indexer.log
-date "+%Y%m%d %H:%M:%S" > $log_file 2>&1
 
 cd /usr/local/rails/toolbox_search/current
+date "+%Y%m%d %H:%M:%S" > $log_file 2>&1
 $bundle_bin exec rake RAILS_ENV=production thinking_sphinx:index >> $log_file 2>&1
