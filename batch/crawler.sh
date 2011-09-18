8695012331f5d8c5e68e4c3738edeb6e21bbd2fc
rochefort <teraswan@gmail.com>
2011-09-18 16:24:07 +0900

    mistake name of log file

--- batch/crawler.sh
+++ batch/crawler.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/local/bin/:$PATH
 bundle_bin=/home/trsw/.gem/ruby/1.9.1/bin/bundle
 
-log_file=log/cron_indexer.log
+log_file=log/cron_crawler.log
 date "+%Y%m%d %H:%M:%S" > $log_file 2>&1
 
 cd /usr/local/rails/toolbox_search/current
