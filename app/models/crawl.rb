046a502d3a655933588162a6652bcf887610258c
rochefort <teraswan@gmail.com>
2011-09-25 02:00:06 +0900

    define default_scope for crawl

--- app/models/crawl.rb
+++ app/models/crawl.rb
@@ -1,14 +1,12 @@
 class Crawl < ActiveRecord::Base
-  scope :active, :conditions => {:result => 1}
-  
+  default_scope where(:result => 1)
   has_many :categories
   def self.max_id
-    self.active.maximum('id')
-    #maximum('id', :conditions => { :result => 1 })
+    self.maximum('id')
   end
   
   def self.before_id(base_id, num_old_day)
     before_day = num_old_day.days.ago(self.find(base_id).created_at.to_date)
-    self.active.find(:first, :conditions => ["created_at >= ?", before_day]).id
+    self.find(:first, :conditions => ['created_at >= ?', before_day]).id
   end
 end
